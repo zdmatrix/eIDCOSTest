ac7d5d76759202b73307b7a1a5e64b3bfcb2d8ff
zdmatrix <zdmatrix@163.com>
2014-07-23 10:53:24 +0800

    realise the combox down list show and select the specifice item

--- eIDCOSTest/GUI/Form1.h
+++ eIDCOSTest/GUI/Form1.h
@@ -41,13 +41,35 @@ namespace GUI {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::ListBox^  listBox1;
+	private: System::Windows::Forms::TabControl^  tabControl1;
+	protected: 
+
+	protected: 
+
+	private: System::Windows::Forms::TabPage^  performanceTest;
+	private: System::Windows::Forms::TabPage^  applicateTest;
 	private: System::Windows::Forms::Label^  label1;
-	private: System::Windows::Forms::Button^  button1;
-	private: System::Windows::Forms::Button^  button2;
-	private: System::Windows::Forms::Button^  button3;
-	private: System::Windows::Forms::Label^  label2;
 	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::Label^  label2;
+
+	private: System::Windows::Forms::Button^  btnSM2Verify;
+	private: System::Windows::Forms::Button^  btnSM2Sign;
+	private: System::Windows::Forms::Button^  btnGeneralRSAKey;
+	private: System::Windows::Forms::ComboBox^  comboBox1;
+
+
+
+
+	protected: 
+
+
+
+
+
+
+
+
+
 	protected: 
 
 	private:
@@ -63,108 +85,178 @@ namespace GUI {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->button2 = (gcnew System::Windows::Forms::Button());
-			this->button3 = (gcnew System::Windows::Forms::Button());
-			this->label2 = (gcnew System::Windows::Forms::Label());
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(Form1::typeid));
+			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
+			this->performanceTest = (gcnew System::Windows::Forms::TabPage());
+			this->btnSM2Verify = (gcnew System::Windows::Forms::Button());
+			this->btnSM2Sign = (gcnew System::Windows::Forms::Button());
+			this->btnGeneralRSAKey = (gcnew System::Windows::Forms::Button());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->applicateTest = (gcnew System::Windows::Forms::TabPage());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->tabControl1->SuspendLayout();
+			this->performanceTest->SuspendLayout();
 			this->SuspendLayout();
 			// 
-			// listBox1
+			// tabControl1
 			// 
-			this->listBox1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left) 
+			this->tabControl1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom) 
+				| System::Windows::Forms::AnchorStyles::Left) 
 				| System::Windows::Forms::AnchorStyles::Right));
-			this->listBox1->FormattingEnabled = true;
-			this->listBox1->ItemHeight = 12;
-			this->listBox1->Location = System::Drawing::Point(90, 13);
-			this->listBox1->Name = L"listBox1";
-			this->listBox1->Size = System::Drawing::Size(370, 28);
-			this->listBox1->TabIndex = 0;
+			this->tabControl1->Controls->Add(this->performanceTest);
+			this->tabControl1->Controls->Add(this->applicateTest);
+			this->tabControl1->Location = System::Drawing::Point(12, 39);
+			this->tabControl1->Name = L"tabControl1";
+			this->tabControl1->SelectedIndex = 0;
+			this->tabControl1->Size = System::Drawing::Size(448, 275);
+			this->tabControl1->TabIndex = 0;
+			// 
+			// performanceTest
+			// 
+			this->performanceTest->BackColor = System::Drawing::Color::LightGray;
+			this->performanceTest->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->performanceTest->Controls->Add(this->btnSM2Verify);
+			this->performanceTest->Controls->Add(this->btnSM2Sign);
+			this->performanceTest->Controls->Add(this->btnGeneralRSAKey);
+			this->performanceTest->Controls->Add(this->textBox1);
+			this->performanceTest->Controls->Add(this->label2);
+			this->performanceTest->Font = (gcnew System::Drawing::Font(L"宋体", 10.5F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(134)));
+			this->performanceTest->ForeColor = System::Drawing::SystemColors::MenuText;
+			this->performanceTest->Location = System::Drawing::Point(4, 21);
+			this->performanceTest->Name = L"performanceTest";
+			this->performanceTest->Padding = System::Windows::Forms::Padding(3);
+			this->performanceTest->Size = System::Drawing::Size(440, 250);
+			this->performanceTest->TabIndex = 0;
+			this->performanceTest->Text = L"载体性能测试";
+			// 
+			// btnSM2Verify
+			// 
+			this->btnSM2Verify->Location = System::Drawing::Point(308, 25);
+			this->btnSM2Verify->Name = L"btnSM2Verify";
+			this->btnSM2Verify->Size = System::Drawing::Size(107, 23);
+			this->btnSM2Verify->TabIndex = 4;
+			this->btnSM2Verify->Text = L"SM2验签";
+			this->btnSM2Verify->UseVisualStyleBackColor = true;
+			// 
+			// btnSM2Sign
+			// 
+			this->btnSM2Sign->Location = System::Drawing::Point(165, 25);
+			this->btnSM2Sign->Name = L"btnSM2Sign";
+			this->btnSM2Sign->Size = System::Drawing::Size(107, 23);
+			this->btnSM2Sign->TabIndex = 3;
+			this->btnSM2Sign->Text = L"SM2签名";
+			this->btnSM2Sign->UseVisualStyleBackColor = true;
+			// 
+			// btnGeneralRSAKey
+			// 
+			this->btnGeneralRSAKey->BackColor = System::Drawing::Color::LightGray;
+			this->btnGeneralRSAKey->Location = System::Drawing::Point(27, 25);
+			this->btnGeneralRSAKey->Name = L"btnGeneralRSAKey";
+			this->btnGeneralRSAKey->Size = System::Drawing::Size(107, 23);
+			this->btnGeneralRSAKey->TabIndex = 2;
+			this->btnGeneralRSAKey->Text = L"生成RSA公私钥";
+			this->btnGeneralRSAKey->UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this->textBox1->BackColor = System::Drawing::SystemColors::Control;
+			this->textBox1->ForeColor = System::Drawing::Color::Blue;
+			this->textBox1->Location = System::Drawing::Point(7, 80);
+			this->textBox1->Multiline = true;
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->ReadOnly = true;
+			this->textBox1->ScrollBars = System::Windows::Forms::ScrollBars::Vertical;
+			this->textBox1->Size = System::Drawing::Size(427, 164);
+			this->textBox1->TabIndex = 1;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->BackColor = System::Drawing::Color::LightGray;
+			this->label2->Font = (gcnew System::Drawing::Font(L"宋体", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(134)));
+			this->label2->Location = System::Drawing::Point(6, 61);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(72, 16);
+			this->label2->TabIndex = 0;
+			this->label2->Text = L"测试结果";
+			// 
+			// applicateTest
+			// 
+			this->applicateTest->Font = (gcnew System::Drawing::Font(L"宋体", 10.5F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(134)));
+			this->applicateTest->Location = System::Drawing::Point(4, 21);
+			this->applicateTest->Name = L"applicateTest";
+			this->applicateTest->Padding = System::Windows::Forms::Padding(3);
+			this->applicateTest->Size = System::Drawing::Size(440, 250);
+			this->applicateTest->TabIndex = 1;
+			this->applicateTest->Text = L"应用检测";
+			this->applicateTest->UseVisualStyleBackColor = true;
 			// 
 			// label1
 			// 
 			this->label1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left) 
 				| System::Windows::Forms::AnchorStyles::Right));
 			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"宋体", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+			this->label1->Font = (gcnew System::Drawing::Font(L"宋体", 10.5F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
 				static_cast<System::Byte>(134)));
-			this->label1->Location = System::Drawing::Point(12, 25);
+			this->label1->Location = System::Drawing::Point(13, 16);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(72, 16);
+			this->label1->Size = System::Drawing::Size(63, 14);
 			this->label1->TabIndex = 1;
 			this->label1->Text = L"选择设备";
 			// 
-			// button1
+			// comboBox1
 			// 
-			this->button1->Location = System::Drawing::Point(153, 66);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
-			this->button1->TabIndex = 2;
-			this->button1->Text = L"button1";
-			this->button1->UseVisualStyleBackColor = true;
-			// 
-			// button2
-			// 
-			this->button2->Location = System::Drawing::Point(153, 105);
-			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(75, 23);
-			this->button2->TabIndex = 3;
-			this->button2->Text = L"button2";
-			this->button2->UseVisualStyleBackColor = true;
-			// 
-			// button3
-			// 
-			this->button3->Location = System::Drawing::Point(153, 144);
-			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(75, 23);
-			this->button3->TabIndex = 4;
-			this->button3->Text = L"button3";
-			this->button3->UseVisualStyleBackColor = true;
-			// 
-			// label2
-			// 
-			this->label2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left) 
-				| System::Windows::Forms::AnchorStyles::Right));
-			this->label2->AutoSize = true;
-			this->label2->Font = (gcnew System::Drawing::Font(L"宋体", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
-				static_cast<System::Byte>(134)));
-			this->label2->Location = System::Drawing::Point(12, 180);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(72, 16);
-			this->label2->TabIndex = 5;
-			this->label2->Text = L"测试结果";
-			// 
-			// textBox1
-			// 
-			this->textBox1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left) 
-				| System::Windows::Forms::AnchorStyles::Right));
-			this->textBox1->Location = System::Drawing::Point(15, 209);
-			this->textBox1->Multiline = true;
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(445, 105);
-			this->textBox1->TabIndex = 6;
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Location = System::Drawing::Point(83, 13);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(377, 20);
+			this->comboBox1->TabIndex = 2;
+			this->comboBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &Form1::comboBox1_SelectedIndexChanged);
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 12);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::SystemColors::GradientInactiveCaption;
 			this->ClientSize = System::Drawing::Size(472, 326);
-			this->Controls->Add(this->textBox1);
-			this->Controls->Add(this->label2);
-			this->Controls->Add(this->button3);
-			this->Controls->Add(this->button2);
-			this->Controls->Add(this->button1);
+			this->Controls->Add(this->comboBox1);
 			this->Controls->Add(this->label1);
-			this->Controls->Add(this->listBox1);
+			this->Controls->Add(this->tabControl1);
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^  >(resources->GetObject(L"$this.Icon")));
 			this->Name = L"Form1";
-			this->Text = L"eID载体性能测试";
+			this->Text = L"eID应用";
+			this->Shown += gcnew System::EventHandler(this, &Form1::From1_Shown);
+			this->tabControl1->ResumeLayout(false);
+			this->performanceTest->ResumeLayout(false);
+			this->performanceTest->PerformLayout();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+	
+
+			 private:System::Void From1_Shown(System::Object^ sender, System::EventArgs^ s){
+
+						 for ( int x = 1; x <= 50; x++ )
+						 {
+							 comboBox1->Items->Add( String::Format( "Item {0}", x ) );
+
+						 }
+						 
+					 }
+
+private: System::Void comboBox1_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+			 if(comboBox1->SelectedItem->Equals((String^)"Item 1")){
+				textBox1->Text = "测试成功";
+			 }
+
+		 }
+};
 }
 
